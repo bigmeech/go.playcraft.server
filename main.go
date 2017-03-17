62ca2ed532043c1dc3f1f1cb3b7430b82803359e
Larry Eliemenye <larry.eliemenye@tuitravel.com>
2017-03-17 09:04:48 +0000

    setup api endpoints

--- main.go
+++ main.go
@@ -21,9 +21,8 @@ func defaultHandler(rw http.ResponseWriter, req *http.Request){
 
 func main(){
 	mux := bone.New()
-	mux.Get("/", http.HandlerFunc(defaultHandler))
-	mux.Get("/api", http.HandlerFunc(defaultHandler))
-	mux.Get("/api/team/:id", http.HandlerFunc(TeamHandler))
+
+	configRoutes(mux)
 
 	DB_USERNAME := flag.String("db_username", "default_username", "Username to use for database login")
 	DB_PASSWORD := flag.String("db_password", "default_password", "Password to use for database login")
@@ -37,4 +36,69 @@ func main(){
 	// starting api server
 	log.Debug("Starting go.playcraft.server at 8080");
 	http.ListenAndServe(":8080", mux)
+}
+
+
+func userHandler(rw http.ResponseWriter, req *http.Request){
+	defaultHandler(rw , req)
+}
+
+func teamHandler(rw http.ResponseWriter, req *http.Request){
+	defaultHandler(rw , req)
+}
+
+func stratgegyHandler(rw http.ResponseWriter, req *http.Request){
+	defaultHandler(rw , req)
+}
+
+func catchAll(rw http.ResponseWriter, req *http.Request){
+	defaultHandler(rw , req);
+}
+
+
+//Configure routes
+func configRoutes(mux bone.Mux){
+
+	//INDEX
+	mux.Get("/", http.HandlerFunc(defaultHandler))
+	mux.Get("/api", http.HandlerFunc(defaultHandler))
+
+	//USER API
+	mux.Get("/api/user/", http.HandlerFunc(userHandler))
+	mux.Get("/api/user/:id", http.HandlerFunc(userHandler))
+
+	mux.Put("/api/user/", http.HandlerFunc(userHandler))
+	mux.Put("/api/user/:id", http.HandlerFunc(userHandler))
+
+	mux.Post("/api/user", http.HandlerFunc(userHandler))
+	mux.Post("/api/user/:id", http.HandlerFunc(userHandler))
+
+	mux.Delete("/api/user", http.HandlerFunc(userHandler))
+	mux.Delete("/api/user/:id", http.HandlerFunc(userHandler))
+
+	//TEAM API
+	mux.Get("/api/team/", http.HandlerFunc(teamHandler))
+	mux.Get("/api/team/:id", http.HandlerFunc(teamHandler))
+
+	mux.Put("/api/team/", http.HandlerFunc(teamHandler))
+	mux.Put("/api/team/:id", http.HandlerFunc(teamHandler))
+
+	mux.Post("/api/team", http.HandlerFunc(teamHandler))
+	mux.Post("/api/team/:id", http.HandlerFunc(teamHandler))
+
+	mux.Delete("/api/team", http.HandlerFunc(teamHandler))
+	mux.Delete("/api/team/:id", http.HandlerFunc(teamHandler))
+
+	//STRATEGY API
+	mux.Get("/api/strategy/", http.HandlerFunc(stratgegyHandler))
+	mux.Get("/api/strategy/:id", http.HandlerFunc(stratgegyHandler))
+
+	mux.Put("/api/strategy/", http.HandlerFunc(stratgegyHandler))
+	mux.Put("/api/strategy/:id", http.HandlerFunc(stratgegyHandler))
+
+	mux.Post("/api/strategy", http.HandlerFunc(stratgegyHandler))
+	mux.Post("/api/strategy/:id", http.HandlerFunc(stratgegyHandler))
+
+	mux.Delete("/api/strategy", http.HandlerFunc(stratgegyHandler))
+	mux.Delete("/api/strategy/:id", http.HandlerFunc(stratgegyHandler))
 }
